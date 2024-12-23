ceb59731bc8f183d05bc3acf0e621b2483ed518e
islam said <islam.said.dev@gmail.com>
2024-12-23 15:38:07 +0200

    Handle transfer money

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -1,7 +1,9 @@
+
 package db
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/jackc/pgx/v5/pgtype"
@@ -10,32 +12,39 @@ import (
 
 func TestTransferTx(t *testing.T) {
 	ctx := context.Background()
+
 	store := NewStore(testDbPool)
 	account1 := createRandomAccount(t)
 	account2 := createRandomAccount(t)
+	fmt.Println(">> before:", account1.Balance, account2.Balance)
+
+	// run n concurrent transfer transactions
 	n := 5
 	amount := int64(10)
-
 	errs := make(chan error)
 	results := make(chan TransferTxResult)
-	// run n concurrent transfer transaction
-	for i := 0; i < n; i++ {
 
+	for i := 0; i < n; i++ {
 		go func() {
+			ctx := context.Background()
 			result, err := store.TransferTx(ctx, TransferTxParams{
 				FromAccountID: pgtype.Int8{Int64: account1.ID, Valid: true},
 				ToAccountID:   pgtype.Int8{Int64: account2.ID, Valid: true},
 				Amount:        amount,
 			})
-
 			errs <- err
 			results <- result
 		}()
 	}
-
-	for i := 0; i < n; i++ {
+	// check results
+	existed := make(map[int]bool)
+	for i := 0; i < n; i++ { 
 		err := <-errs
+		require.NoError(t, err)
+
 		result := <-results
+		require.NotEmpty(t, result)
+
 		// check transfer
 		transfer := result.Transfer
 		require.Equal(t, int64(transfer.FromAccountID.Int64), account1.ID)
@@ -67,5 +76,77 @@ func TestTransferTx(t *testing.T) {
 
 		_, err = store.GetEntry(ctx, toEntry.ID)
 		require.NoError(t, err)
+
+		// check accounts
+		fromAccount := result.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, account1.ID, fromAccount.ID)
+
+		toAccount := result.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, account2.ID, toAccount.ID)
+
+		// check balances
+		fmt.Println(">> tx:", fromAccount.Balance, toAccount.Balance)
+
+		diff1 := account1.Balance - fromAccount.Balance
+		diff2 := toAccount.Balance - account2.Balance
+		require.Equal(t, diff1, diff2)
+		require.True(t, diff1 > 0)
+		require.True(t, diff1%amount == 0) // 1 * amount, 2 * amount, 3 * amount, ..., n * amount
+
+		k := int(diff1 / amount)
+		require.True(t, k >= 1 && k <= n)
+		require.NotContains(t, existed, k)
+		existed[k] = true
 	}
+}
+
+func TestTransferTxDeadlock(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	ctx := context.Background()
+	store := NewStore(testDbPool)
+
+	fmt.Println(">> before:", account1.Balance, account2.Balance)
+	n := 10
+	amount := int64(10)
+	errs := make(chan error)
+
+	for i := 0; i < n; i++ {
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		if i%2 == 1 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
+
+		go func() {
+			_, err := store.TransferTx(ctx, TransferTxParams{
+				FromAccountID: pgtype.Int8{Int64: fromAccountID, Valid: true},
+				ToAccountID:   pgtype.Int8{Int64: toAccountID, Valid: true},
+				Amount:        amount,
+			})
+			errs <- err
+
+		}()
+	}
+
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+	}
+
+	// check the final updated balance
+	updatedAccount1, err := store.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := store.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	fmt.Println(">> after:", updatedAccount1.Balance, updatedAccount2.Balance)
+	require.Equal(t, account1.Balance, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance, updatedAccount2.Balance)
 }
